29fad5b4da4dba28874b54d4d793fe452da15661
iavellas22 <irene.avellas@hotmail.com>
2020-03-24 10:06:42 +0100

    Apartado15 No carga bien desde StackBlitz

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
-
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+
+import {HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
